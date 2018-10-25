28b50e24ba792f8419f11410986dd698a0d24eba
rinagorsha <rinagorsha@rccraft.ru>
2018-10-26 01:01:02 +0300

    [cr] управление камерой в отдельном файле

---
+++ src/js/cameraControll.js
@@ -0,0 +1,82 @@
+export default () => {
+  const camera = document.querySelector('.js-camera');
+  const image = document.querySelector('.js-camera-image');
+  const scaleEl = document.querySelector('.js-camera-scale');
+  const brightnessEl = document.querySelector('.js-camera-brightness');
+
+  if (!camera || !image || !scaleEl || !brightnessEl) return;
+
+  let evCache = [];
+  let gesture = null;
+  let prevScaleDiff = -1;
+  let currentScale = 1;
+
+  const SCALE_MULTIPLIER = 8000;
+  const PINCH_VS_SCALE = 3;
+  const MAX_SCALE = 2;
+
+  camera.addEventListener('pointerdown', (event) => {
+    camera.setPointerCapture(event.pointerId);
+    evCache.push(event);
+
+    const currentX = parseInt(getComputedStyle(image).backgroundPositionX, 10) || 0;
+    const currentY = parseInt(getComputedStyle(image).backgroundPositionY, 10) || 0;
+
+    gesture = {
+      startX: event.x - currentX,
+      startY: event.y - currentY,
+    };
+  });
+
+  camera.addEventListener('pointermove', (event) => {
+    if (!gesture) {
+      return;
+    }
+
+    // drag
+    if (evCache.length === 1) {
+      const dx = gesture.startX - event.x;
+      const dy = gesture.startY - event.y;
+
+      image.style.backgroundPosition = `${-dx}px ${-dy}px`;
+    }
+
+    if (evCache.length === 2) {
+      // brightness
+      const dx = Math.abs(evCache[0].x - evCache[1].x);
+      const dy = Math.abs(evCache[0].y - evCache[1].y);
+      const diffAtan = Math.atan2(dy, dx);
+      image.style.filter = `brightness(${diffAtan})`;
+      brightnessEl.innerText = `${Math.round(diffAtan * 100)}%`;
+
+      // scale
+      const currentScaleDiff = Math.hypot(dy, dx);
+      if (Math.abs(currentScaleDiff - prevScaleDiff) > PINCH_VS_SCALE) {
+        if (currentScaleDiff > prevScaleDiff) currentScale += currentScaleDiff / SCALE_MULTIPLIER;
+        if (currentScaleDiff < prevScaleDiff) currentScale -= currentScaleDiff / SCALE_MULTIPLIER;
+      }
+      currentScale = Math.min(MAX_SCALE, Math.max(1, currentScale));
+      prevScaleDiff = currentScaleDiff;
+
+      image.style.transform = `scale(${currentScale})`;
+      scaleEl.innerText = `${Math.round(currentScale * 100)}%`;
+    }
+
+    for (let i = 0; i < evCache.length; i++) {
+      if (event.pointerId === evCache[i].pointerId) {
+        evCache[i] = event;
+        break;
+      }
+    }
+  });
+
+  const resetGesture = () => {
+    if (!gesture) {
+      return;
+    }
+    gesture = null;
+    evCache = [];
+  };
+  camera.addEventListener('pointerup', resetGesture);
+  camera.addEventListener('pointercancel', resetGesture);
+};
